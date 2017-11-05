e246c43cb53183387ba82e5dfcf5122af4e2df4b
Kaleem Clarkson <kaleemclarkson@gmail.com>
2017-11-05 15:55:32 -0500

    removal of development settings.php

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -37,15 +37,3 @@ if (file_exists($local_settings)) {
  *
  * See: tests/installer-features/installer.feature
  */
-$settings['install_profile'] = 'standard';
-$databases['default']['default'] = array (
-  'database' => 'drupal8',
-  'username' => 'drupal8',
-  'password' => 'drupal8',
-  'prefix' => '',
-  'host' => 'database',
-  'port' => '3306',
-  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
-  'driver' => 'mysql',
-);
-$settings['hash_salt'] = 'pW7RHw65q4MgGW339L3PdNukZZ0psgGUlu8ihWXO2EpgRhaytw34nzOxtU9dDTzIKePwBdp8pw';
